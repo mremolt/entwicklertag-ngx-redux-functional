ad15e73846ea31e4389d5e77f099c5a7db5f249b
Marc Remolt <marc.remolt@dcs-fuerth.de>
2018-02-16 14:16:59 +0100

    feat(browserslist): add browserslistrc

--- config/webpack.config.common.ts
+++ config/webpack.config.common.ts
@@ -6,35 +6,34 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ProgressPlugin = require('webpack/lib/ProgressPlugin');
 const { CommonsChunkPlugin } = require('webpack').optimize;
 const { AngularCompilerPlugin } = require('@ngtools/webpack');
-const {
-  NoEmitOnErrorsPlugin,
-  EnvironmentPlugin,
-  NormalModuleReplacementPlugin
-} = require('webpack');
+const { NoEmitOnErrorsPlugin, EnvironmentPlugin, NormalModuleReplacementPlugin } = require('webpack');
 
 const root = process.cwd();
 const TS_VERSION = require('typescript').version;
 const APP_ENV = process.env.APP_ENV || 'development';
 
-const Environment = require(path.resolve(root, 'src', 'environments', APP_ENV))
-  .default;
+const Environment = require(path.resolve(root, 'src', 'environments', APP_ENV)).default;
 const environment = new Environment();
 
 const cssLoader = [
   {
-    loader: 'css-loader'
+    loader: 'css-loader',
+    options: {
+      minimize: APP_ENV !== 'development',
+      sourceMap: true,
+    },
   },
   {
     loader: 'postcss-loader',
     options: {
       plugins: () => {
         return [require('autoprefixer')];
-      }
-    }
+      },
+    },
   },
   {
-    loader: 'sass-loader'
-  }
+    loader: 'sass-loader',
+  },
 ];
 
 module.exports = {
@@ -42,27 +41,23 @@ module.exports = {
     extensions: ['.ts', '.js'],
     mainFields: ['es2015', 'module', 'main'],
     alias: {
-      'transit-immutable-js': path.resolve(
-        root,
-        'config',
-        'resource-override.js'
-      ),
+      'transit-immutable-js': path.resolve(root, 'config', 'resource-override.js'),
       'date-fns': path.resolve(root, 'config', 'resource-override.js'),
       // tslint:disable-next-line:object-literal-key-quotes
       ajv: path.resolve(root, 'config', 'resource-override.js'),
       // tslint:disable-next-line:object-literal-key-quotes
-      validator: path.resolve(root, 'config', 'resource-override.js')
-    }
+      validator: path.resolve(root, 'config', 'resource-override.js'),
+    },
   },
   entry: {
     polyfills: './src/polyfills.ts',
-    main: './src/main.ts'
+    main: './src/main.ts',
   },
 
   output: {
     filename: '[name].[hash].bundle.js',
     chunkFilename: '[id].chunk.js',
-    path: path.resolve(root, 'dist')
+    path: path.resolve(root, 'dist'),
   },
 
   module: {
@@ -72,43 +67,43 @@ module.exports = {
         use: [
           {
             loader: '@ngtools/webpack',
-            options: { tsConfigPath: path.resolve(root, 'tsconfig.json') }
-          }
-        ]
+            options: { tsConfigPath: path.resolve(root, 'tsconfig.json') },
+          },
+        ],
       },
       {
         test: /\.html$/,
         loader: 'raw-loader',
-        include: [path.resolve(root, 'src', 'app')]
+        include: [path.resolve(root, 'src', 'app')],
       },
       {
         test: /\.(scss)$/,
         use: [
           {
-            loader: 'cache-loader'
+            loader: 'style-loader',
           },
           {
-            loader: 'thread-loader'
+            loader: 'cache-loader',
           },
           {
-            loader: 'style-loader'
+            loader: 'thread-loader',
           },
-          ...cssLoader
+          ...cssLoader,
         ],
-        include: [path.resolve(root, 'src', 'styles')]
+        include: [path.resolve(root, 'src', 'styles')],
       },
 
       {
         test: /\.(scss)$/,
         use: [
           {
-            loader: 'to-string-loader'
+            loader: 'to-string-loader',
           },
-          ...cssLoader
+          ...cssLoader,
         ],
-        include: [path.resolve(root, 'src', 'app')]
-      }
-    ]
+        include: [path.resolve(root, 'src', 'app')],
+      },
+    ],
   },
 
   devtool: 'source-map',
@@ -122,18 +117,18 @@ module.exports = {
       {
         from: '**/*',
         to: 'assets',
-        context: 'src/assets'
+        context: 'src/assets',
       },
       {
         from: 'src/manifest.json',
-        to: ''
+        to: '',
       },
-      { from: 'src/_redirects', to: '' }
+      { from: 'src/_redirects', to: '' },
     ]),
 
     new EnvironmentPlugin({
       TS_VERSION,
-      APP_ENV
+      APP_ENV,
     }),
 
     // load the configuration for the current environment (development, staging, production ...)
@@ -146,19 +141,19 @@ module.exports = {
       template: 'src/index.ejs',
       chunks: ['polyfills', 'vendor', 'main', 'offline'],
       chunksSortMode: 'manual',
-      environment
+      environment,
     }),
 
     new CommonsChunkPlugin({
       name: 'vendor',
       chunks: ['offline', 'main'],
-      minChunks: (module: any) => /node_modules/.test(module.resource)
+      minChunks: (module: any) => /node_modules/.test(module.resource),
     }),
 
     new AngularCompilerPlugin({
       mainPath: 'src/main.ts',
       tsConfigPath: 'tsconfig.json',
-      sourceMap: true
-    })
-  ]
+      sourceMap: true,
+    }),
+  ],
 };
